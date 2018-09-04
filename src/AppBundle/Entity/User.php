f7b91d453b2d63e9e2e01c63ff95104c62992ac4
Ivan Kudryavsky <ivan.kudryavsky@requestum.com>
2018-09-04 14:23:43 +0300

    Remove nullable flag

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -54,7 +54,7 @@ class User implements UserInterface
 
     /**
      * @var string Encrypted password.
-     * @ORM\Column(type="string", nullable=true)
+     * @ORM\Column(type="string")
      *
      */
     protected $password;
